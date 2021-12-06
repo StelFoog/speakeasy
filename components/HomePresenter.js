71bd579090b3e2b41a5712da4ed37ddc4c2c72e0
HannesSundin <hannessu@kth.se>
2021-12-06 12:12:19 +0100

    Update HomePresenter.js

--- components/HomePresenter.js
+++ components/HomePresenter.js
@@ -1,69 +1,37 @@
 import {useState, Fragment} from "react";
-import {LoginViews, FormStateRadioView, StaffTypeRadioView, SubmitButtonView, LoginRegisterForm} from "./HomeViews";
-import styles from "../styles/Home.module.css";
-import Head from "next/head";
+import {SubmitButtonView, LoginForm} from "./HomeViews";
 import WelcomeView from "./WelcomeView";
-import {btoa} from "next/dist/server/web/sandbox/polyfills";
+import bcrypt from 'bcrypt';
+import {SALT_ROUNDS} from "../util/db";
+
 
 function HomePresenter(props) {
-    const [formState, setFormState] = useState("login")
-    const [name, setName] = useState('');
     const [password, setPassword] = useState('');
-    const [repeatPassword, setRepeatPassword] = useState('');
-    const [staffType, setStaffType] = useState('')
     const [pnr, setPnr] = useState('')
     return (
         <WelcomeView components={
             <Fragment>
-                <FormStateRadioView
-                    onFormStateChange={(change) => {
-                        setFormState(change)
-                    }}>
-                </FormStateRadioView>
-                <br/>
-                {formState === "register" &&
-                <StaffTypeRadioView
-                    staffType={staffType}
-                    onStaffTypeChange={setStaffType}/>
-                }
-                <LoginRegisterForm
+                <LoginForm
                     password={password}
-                    username={name}
                     pnr={pnr}
-                    repeatPassword={repeatPassword}
                     onPnrChange={setPnr}
-                    onNameChange={setName}
-                    onPasswordChange={setPassword}
-                    onRepeatPasswordChange={setRepeatPassword}
-                    formState={formState}>
-
-                </LoginRegisterForm>
+                    onPasswordChange={setPassword}>
+                </LoginForm>
                 <SubmitButtonView
-                    formState={formState}
-                    onLogin={() => {
-                    }
-                    }
-                    onRegister={async () => {
-                        const result = await fetch('/api/staff/create', {
-                            method: 'PUT',
+                    action={"Login"}
+                    onSubmit={async () => {
+                        const result = await fetch('/api/staff/login', {
+                            method: 'GET',
                             headers: {
                                 'Content-Type': 'application/json',
-                                'Authorization': `${name}+${password}`
+                                'Authorization': `${pnr}+${bcrypt.hash(password, SALT_ROUNDS)}`
                             },
-                            body: JSON.stringify({
-                                "name": name,
-                                pnr,
-                                "plainPassword": password,
-                                "type": staffType
-                            })
-
                         })
-                    }}
-                    isDisabled={
-                        formState === 'register' ?
-                            !staffType || !name || !password || !repeatPassword || !pnr  || pnr.length < 10 && password !== repeatPassword :
-                            !pnr || !password || pnr.length < 10
-                    }>
+
+                        console.log(result)
+                    }
+                    }
+                    isDisabled={!pnr || !password || pnr.length !== 10}>
                 </SubmitButtonView>
             </Fragment>
         }/>
