2fa65b88da86846860aa25d53678a350b6bfe7da
samuel <samuel@familjenlarsson.eu>
2021-12-06 16:25:27 +0100

    fixed missing newline at eof and some formatting

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,10 @@
-import {configureStore} from "@reduxjs/toolkit";
-import userReducer from "./user";
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './user';
 
 const store = configureStore({
-  reducer: {
-    user: userReducer
-  }
+	reducer: {
+		user: userReducer,
+	},
 });
 
 export default store;
--- redux/user.js
+++ redux/user.js
@@ -1,31 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  name: null,
-  pnr: null,
-  password: null,
-  type: null,
-}
+	name: null,
+	pnr: null,
+	password: null,
+	type: null,
+};
 
 export const userSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    setUser(state, {payload}) {
-      state.name = payload.name;
-      state.pnr = payload.pnr;
-      state.password = payload.password;
-      state.type = payload.type;
-    },
-    removeUser(state) {
-      state.name = null;
-      state.pnr = null;
-      state.password = null;
-      state.type = null;
-    }
-  }
+	name: 'user',
+	initialState,
+	reducers: {
+		setUser(state, { payload }) {
+			state.name = payload.name;
+			state.pnr = payload.pnr;
+			state.password = payload.password;
+			state.type = payload.type;
+		},
+		removeUser(state) {
+			state.name = null;
+			state.pnr = null;
+			state.password = null;
+			state.type = null;
+		},
+	},
 });
 
-export const {setUser, removeUser} = userSlice.actions;
+export const { setUser, removeUser } = userSlice.actions;
 export const selectUser = (state) => state.user;
 export default userSlice.reducer;
