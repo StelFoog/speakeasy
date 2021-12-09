8b9c332fdd7fc8eea3757011de58ecf56c1652d2
samuel <samuel@familjenlarsson.eu>
2021-12-09 16:52:15 +0100

    finished ui of new report form

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -5,7 +5,7 @@ import { selectUser } from '../redux/user';
 import { lastWeekDates } from '../util/dates';
 import styles from '../styles/Dashboard.module.css';
 import DaySelector from '../components/DaySelector';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function MetaData({ name = 'Name Namesson' }) {
 	return (
@@ -16,13 +16,52 @@ function MetaData({ name = 'Name Namesson' }) {
 	);
 }
 
-function NewReport({ makeReport, selectedDate, setSelectedDate }) {
+function NewReport({
+	makeReport,
+	selectedDate,
+	setSelectedDate,
+	hours,
+	setHours,
+	setNote,
+	disableSubmit,
+}) {
 	const dates = lastWeekDates();
 
 	return (
 		<div className={styles.newReport}>
 			<Box>
-				<DaySelector dates={dates} selected={selectedDate} onChange={setSelectedDate} />
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+						if (!disableSubmit) makeReport();
+					}}
+				>
+					<DaySelector dates={dates} selected={selectedDate} onChange={setSelectedDate} />
+					<table>
+						<tbody>
+							<tr>
+								<td>
+									<label>Hours:</label>
+								</td>
+								<td>
+									<input value={hours} onInput={(e) => setHours(e.target.value)} placeholder="0" />
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<label>Note:</label>
+								</td>
+								<td>
+									<textarea onChange={(e) => setNote(e.target.value)} />
+								</td>
+							</tr>
+						</tbody>
+					</table>
+
+					<button type="submit" disabled={disableSubmit}>
+						Report
+					</button>
+				</form>
 			</Box>
 		</div>
 	);
@@ -42,19 +81,30 @@ export default function Dashboard() {
 
 	// new report form values
 	const [date, setDate] = useState('');
+	const [hours, setHours] = useState('');
+	const [note, setNote] = useState('');
 
-	// TEMP: log changes to the form values
-	useEffect(() => {
-		console.log('date:', date);
-	}, [date]);
+	function setHoursWorked(input) {
+		setHours(input.replace(/\D/g, ''));
+	}
 
-	function makeNewReport() {}
+	function makeNewReport() {
+		console.log({ date, hours, note });
+	}
 
 	return (
 		<>
 			<MetaData name={user.name} />
 			<main className={styles.container}>
-				<NewReport makeReport={makeNewReport} selectedDate={date} setSelectedDate={setDate} />
+				<NewReport
+					makeReport={makeNewReport}
+					selectedDate={date}
+					setSelectedDate={setDate}
+					hours={hours}
+					setHours={setHoursWorked}
+					setNote={setNote}
+					disableSubmit={!date || !hours}
+				/>
 				<OldReports />
 			</main>
 		</>
