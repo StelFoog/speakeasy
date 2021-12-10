7318fa7cdc1dcc7e5ce60e2dc8b84d96de73ed50
samuel <samuel@familjenlarsson.eu>
2021-12-10 14:13:45 +0100

    added modification of reports and renamed endpoint

--- pages/api/reports/insert.js
+++ pages/api/reports/insert.js
@@ -15,7 +15,8 @@ function validNewReportObject(report) {
 // Checks that there's a body that's a valid staff object and that the user creating it is a manager. Then creates the new user.
 export default async function handler(req, res) {
 	const { method, headers, body } = req;
-	if (method !== 'PUT') return res.status(405).json({ error: `Method ${method} not allowed` });
+	if (method !== 'PUT' && method !== 'POST')
+		return res.status(405).json({ error: `Method ${method} not allowed` });
 
 	const { authorization } = headers;
 	if (!authorization) return res.status(401).json({ error: 'Not authorization provided' });
@@ -31,7 +32,19 @@ export default async function handler(req, res) {
 	const report = { date, hours, note: reportingNote, pnr: authPnr(authorization) };
 
 	const { db } = await connectToDatabase();
-	const result = await db.collection('reports').insertOne(report);
+
+	const existingReport = await db
+		.collection('reports')
+		.findOne({ pnr: authPnr(authorization), date });
+	if (existingReport && method === 'PUT')
+		return res.status(400).json({ error: 'A report for this date already exists' });
+	if (!existingReport && method === 'POST')
+		return res.status(400).json({ error: 'No existing report to update' });
+
+	const result =
+		method === 'PUT'
+			? await db.collection('reports').insertOne(report)
+			: await db.collection('reports').updateOne({ _id: existingReport._id }, { $set: report });
 
 	res.status(200).json({ result });
 }
