42f9c827bd40187012bb11dca8a672640d6e18c2
hannessu <hannessu@kth.se>
2021-12-15 17:26:16 +0100

    requested changes

--- pages/members.js
+++ pages/members.js
@@ -5,33 +5,34 @@ import {selectUser} from "../redux/user";
 import styles from '../styles/Members.module.css'
 
 function MembersList({members}) {
-    return (
-        <div className={styles.container}>
-            {members.map(({_id, name, pnr, inside}) => {
-                return <div key={_id}>
-                    <Member name={name} pnr={pnr} status={inside ? "INSIDE" : ""}/>
-                </div>
-            })}
-        </div>)
+  return (
+    <main>
+      <div className={styles.container}>
+        {members.map(({_id, name, pnr, inside}) => {
+          return <div key={_id}>
+            <Member name={name} pnr={pnr} status={inside ? "INSIDE" : ""}/>
+          </div>
+        })}
+      </div>
+    </main>
+  )
 }
 
 export default function Members() {
-    const [members, setMembers] = useState([])
-    const user = useSelector(selectUser)
-    useEffect(() => {
-        const baseURL = (process.env.NODE_ENV === "development" ?
-            "http://localhost:3000/" : "https://speakeasy-ten.vercel.app/")
-        fetch(baseURL + "api/members/get", {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `${user.pnr}+${user.password}`,
-            }
-        })
-            .then(res => res.json())
-            .then(data => setMembers(data))
-            .catch((err) => console.log(err.statusText));
-    }, [])
-    return <MembersList members={members}/>
+  const [members, setMembers] = useState([])
+  const user = useSelector(selectUser)
+  useEffect(() => {
+    fetch("api/members/get", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${user.pnr}+${user.password}`,
+      }
+    })
+      .then(res => res.json())
+      .then(data => setMembers(data))
+      .catch((err) => console.log(err.statusText));
+  }, [])
+  return <MembersList members={members}/>
 }
 
