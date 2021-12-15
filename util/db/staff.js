5afbecd928a90cf828d859ecfb232ef4c1fde063
samuel <samuel@familjenlarsson.eu>
2021-12-15 17:26:40 +0100

    added function for staff endpoints

---
+++ util/db/staff.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+function instance(pnr, password) {
+	return axios.create({
+		baseURL: '/api/staff/',
+		headers: {
+			Authorization: `${pnr}+${password}`,
+		},
+	});
+}
+
+export async function login({ pnr, password }) {
+	return instance(pnr, password)
+		.get('/login')
+		.then((res) => ({ ...res.data, pnr, password }))
+		.catch((error) => console.error(error));
+}
+
+export async function create({ pnr, password }, staffMember) {
+	return instance(pnr, password)
+		.put('/create', staffMember)
+		.then((res) => res.data.result)
+		.catch((error) => console.error(error));
+}
+
+export async function getOwnData({ pnr, password }) {
+	return instance(pnr, password)
+		.get('/get')
+		.then((res) => res.data)
+		.catch((error) => console.error(error));
+}
+
+export async function getStaffData({ pnr, password }, staffPnr) {
+	return instance(pnr, password)
+		.get(`/get/${staffPnr}`)
+		.then((res) => res.data)
+		.catch((error) => console.error(error));
+}
