8638fd40737bfef68e8848f3b9d9a6ac4301087e
HannesSundin <hannessu@kth.se>
2021-12-15 10:26:32 +0100

    Create members.js

---
+++ pages/members.js
@@ -0,0 +1,39 @@
+import {useEffect, useState} from "react";
+import Member from "../components/Member";
+import {useSelector} from "react-redux";
+import {selectUser} from "../redux/user";
+
+function MembersList({members}) {
+    return (
+        <>
+            {members.map(({_id, name, pnr, inside}) => {
+                return <div key={_id}>
+                    <Member name={name} pnr={pnr} status={inside ? "INSIDE" : ""}/>
+                </div>
+            })}
+        </>)
+}
+
+export default function Members() {
+    const [members, setMembers] = useState([])
+    const user = useSelector(selectUser)
+    useEffect(() => {
+        const baseURL = (process.env.NODE_ENV === "development" ?
+            "http://localhost:3000/" : "https://speakeasy-ten.vercel.app/")
+        fetch(baseURL + "api/members/get", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `${user.pnr}+${user.password}`,
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                return setMembers(data)
+            })
+            .catch((err) => console.log(err.statusText));
+    }, [])
+    return <MembersList members={members}/>
+}
+
