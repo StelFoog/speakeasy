8abf8467a72907b7523a4ef5f5b8618ed4bce294
samuel <samuel@familjenlarsson.eu>
2021-12-15 11:24:46 +0100

    added getDrinkFromId function

--- util/cocktail.js
+++ util/cocktail.js
@@ -1,14 +1,23 @@
 import axios from 'axios';
 
 async function baseCall(query) {
-	return axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?${query}`);
+	return axios.get(`https://www.thecocktaildb.com/api/json/v1/1/${query}`);
 }
 
 export async function searchDrinks(query) {
-	return baseCall(`s=${query}`)
+	return baseCall(`search.php?s=${query}`)
 		.then((res) => {
 			if (res.status !== 200) throw res.statusText;
 			return res.data.drinks;
 		})
 		.catch((reason) => console.error(reason));
+}
+
+export async function getDrinkFromId(id) {
+	return baseCall(`lookup.php?i=${id}`).then((res) => {
+		if (res.status !== 200) throw res.statusText;
+
+		if (res.data.drinks === null) return null;
+		else return res.data.drinks[0];
+	});
 }
