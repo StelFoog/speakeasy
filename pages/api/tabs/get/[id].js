acaff709ff8411665cca9b1251e0773f26bad8da
samuel <samuel@familjenlarsson.eu>
2021-12-16 18:26:23 +0100

    began work on tab page

--- pages/api/tabs/get/[id].js
+++ pages/api/tabs/get/[id].js
@@ -7,17 +7,31 @@ export default async function handler(req, res) {
 	const { method, headers, query } = req;
 	if (method !== 'GET') return res.status(405).json({ error: `Method ${method} not allowed` });
 
-	const { authorization } = headers;
-	if (!authorization) return res.status(401).json({ error: 'Not authorization provided' });
-	// Verify authorization
-	const { authorized, status, data } = await authorize(authorization);
-	if (!authorized) return res.status(status).json(data);
+	const { id, exists } = query;
 
-	const { db } = await connectToDatabase();
-	const { id } = query;
+	if (id.length !== 24) return res.status(400).json({ error: `Invalid id` });
 
-	const tabData = await db.collection('tabs').findOne({ _id: new ObjectId(id) });
-	if (!tabData) return res.status(400).json({ error: "This tab doesn't exist" });
+	if (typeof exists !== 'undefined') {
+		// only check if tab exists
+		const { db } = await connectToDatabase();
 
-	res.status(200).json(tabData);
+		const tabData = await db.collection('tabs').findOne({ _id: new ObjectId(id) }, { _id: 1 });
+		if (!tabData) return res.status(400).json({ error: "This tab doesn't exist" });
+
+		res.status(200).json(tabData);
+	} else {
+		// get tab data
+		const { authorization } = headers;
+		if (!authorization) return res.status(401).json({ error: 'Not authorization provided' });
+		// Verify authorization
+		const { authorized, status, data } = await authorize(authorization);
+		if (!authorized) return res.status(status).json(data);
+
+		const { db } = await connectToDatabase();
+
+		const tabData = await db.collection('tabs').findOne({ _id: new ObjectId(id) });
+		if (!tabData) return res.status(400).json({ error: "This tab doesn't exist" });
+
+		res.status(200).json(tabData);
+	}
 }
