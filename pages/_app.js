6a190ce670ed9ad809ea96eb3b32826bab966e8b
samuel <samuel@familjenlarsson.eu>
2021-12-18 20:38:30 +0100

    added verification of signin

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,18 @@ import { Provider } from 'react-redux';
 import SideBar from '../components/SideBar';
 import store, { persistor } from '../redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import LoginVerified from '../components/LoginVerified';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
-        <SideBar />
-        <section className={'content'}>
-          <Component {...pageProps} />
-				</section>
+				<LoginVerified whitelist={['/']}>
+					<SideBar />
+					<section className={'content'}>
+						<Component {...pageProps} />
+					</section>
+				</LoginVerified>
 			</PersistGate>
 		</Provider>
 	);
