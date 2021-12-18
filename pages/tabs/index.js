7db2165537345858aa7ed0392b2bc5f763d3bc97
samuel <samuel@familjenlarsson.eu>
2021-12-18 13:57:04 +0100

    removed git markers

--- pages/tabs/index.js
+++ pages/tabs/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
@@ -41,27 +40,11 @@ function TabsList({ tabsCollection, loading, error, handleGetMoreTabs }) {
 			{!!error && error}
 			{!loading && !error && <button onClick={handleGetMoreTabs}>More tabs</button>}
 		</>
-=======
-import { useRouter } from 'next/dist/client/router';
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { selectUser } from '../../redux/user';
-import { getTabs } from '../../util/db/tabs';
-
-function TabsList({ tabs }) {
-	return (
-		<ul>
-			{tabs.map((tab) => (
-				<li key={tab._id}>{tab.pnr}</li>
-			))}
-		</ul>
->>>>>>> d6452c4 (added tabs page and added to sidebar)
 	);
 }
 
 export default function Tabs() {
 	const user = useSelector(selectUser);
-<<<<<<< HEAD
 	const [tabsCollection, setTabsCollection] = useState([]);
 	// const [page, setPage] = useState(0);
 	const [loading, setLoading] = useState(null);
@@ -99,34 +82,6 @@ export default function Tabs() {
 				// setPage={setPage}
 				// noMorePages={noMorePages}
 			/>
-=======
-	const [tabs, setTabs] = useState([]);
-	const [loading, setLoading] = useState(true);
-	const [error, setError] = useState(null);
-
-	useEffect(() => {
-		getTabs(user)
-			.then((tabs) => {
-				if (tabs.length < 1) {
-					setError(
-						"There doesn't appear to be any tabs on this page, try reloading the page or going back"
-					);
-				} else {
-					setTabs(tabs);
-					setLoading(false);
-				}
-			})
-			.catch(() => setError('An error occured while getting the tabs'));
-	}, [page]);
-
-	if (error) return <main>{error}</main>;
-
-	if (loading) return <main>Loading...</main>;
-
-	return (
-		<main>
-			<TabsList tabs={tabs} />
->>>>>>> d6452c4 (added tabs page and added to sidebar)
 		</main>
 	);
 }
